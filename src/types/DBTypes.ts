7218d3abff09dcdfb8ea6971006850571be9ac89
Mashadeve <74298578+Mashadeve@users.noreply.github.com>
2024-01-19 19:16:48 +0200

    asd

---
+++ src/types/DBTypes.ts
@@ -0,0 +1,28 @@
+type User = {
+  user_id: number;
+  user_name: string;
+  email: string;
+  role: 'user' | 'admin';
+  password: string;
+};
+
+type Cat = {
+  // TODO: create a cat type
+  // owner should be a User or a number
+  cat_id: number;
+  cat_name: string;
+  weight: number;
+  filename: string;
+  birthdate: string;
+  lat: number;
+  lng: number;
+  owner: User | number;
+  // owner: {
+  //   user_id: number;
+  //   user_name: string;
+  // };
+};
+
+export {Cat};
+
+export {User};
